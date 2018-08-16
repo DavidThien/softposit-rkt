de07e8870848318687513b561baa7300e005738f
DavidThien <dthien@cs.washington.edu>
2018-08-15 17:32:52 -0700

    Fixed bugs in install script and now move foreign lib

--- install.rkt
+++ install.rkt
@@ -1,11 +1,15 @@
 #lang racket/base
 
 (require racket/system)
+(require racket/list)
 
 (define (call-and-wait command)
   (define out (process command))
-  (subprocess-wait (third out)))
+  ;; TODO: This is just for testing purposes
+  (for ([i (in-naturals)]
+        #:break (not (eq? ((fifth out) 'status) 'running)))
+    void))
 
 ;; Compile the softposit C library and wait for the make command to finish
 (call-and-wait "make -C SoftPosit/build/Linux-x86_64-GCC SOFTPOSIT_OPTS=\"$(SOFTPOSIT_OPTS) -fPIC\"")
-(call-and-wait "gcc *.o -shared -o ../../../libsoftposit.so")
+(call-and-wait "gcc SoftPosit/build/Linux-x86_64-Gcc/*.o -shared -o libsoftposit.so")
