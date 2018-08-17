d614efb1cdbf7d0c52279b080c0ced8225eb64bc
DavidThien <dthien@cs.washington.edu>
2018-08-16 22:20:49 -0700

    Debugging

--- install.rkt
+++ install.rkt
@@ -15,7 +15,9 @@
     (println (read (car out))))
 
   (define build-location (build-path this-collection-path "SoftPosit/build/Linux-x86_64-GCC/"))
+  (println build-location)
   (define lib-location (build-path this-collection-path "libsoftposit.so"))
+  (println lib-location)
 
   ;; Compile the softposit C library and wait for the make command to finish
   (call-and-wait (string-append "make -C " (path->string build-location) " SOFTPOSIT_OPTS=\"$(SOFTPOSIT_OPTS) -fPIC\""))
