db1e3f519958e323c6efb8ed6536876170bdc16f
DavidThien <dthien@cs.washington.edu>
2018-08-16 23:07:28 -0700

    Updated installer to reflect switch from git submodule to git subtree

--- install.rkt
+++ install.rkt
@@ -15,15 +15,6 @@
 
   (define build-location (build-path this-collection-path "SoftPosit/build/Linux-x86_64-GCC/"))
   (define lib-location (build-path this-collection-path "libsoftposit.so"))
-  (define softposit-location (build-path this-collection-path "SoftPosit"))
-  (println (path->string softposit-location))
-  (println (string-append "git -C " (path->string softposit-location) " submodule init"))
-
-  ;; If the SoftPosit path is empty, then git submodules weren't initialized
-  (when (= 0 (length (directory-list softposit-location)))
-    (begin
-      (call-and-wait (string-append "git -C " (path->string softposit-location) " submodule init"))
-      (call-and-wait (string-append "git -C " (path->string softposit-location) " submodule update"))))
 
   ;; Compile the softposit C library and wait for the make command to finish
   (call-and-wait (string-append "make -C " (path->string build-location) " SOFTPOSIT_OPTS=\"$(SOFTPOSIT_OPTS) -fPIC\""))
