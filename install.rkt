c377886f1b6013fab50f7929179d9528ad01f032
Pavel Panchekha <me@pavpanchekha.com>
2024-04-08 17:29:00 -0600

    Fix install on macOS
    On macOS, `gcc` is actually `clang`. By default, `SoftPosit` passes `-march=core-avx2`, which Clang doesn't support for some reason. I don't see a reason to optimize for anything other than native.

--- install.rkt
+++ install.rkt
@@ -17,6 +17,6 @@
   (define lib-location (build-path (if user-specific? (find-user-lib-dir) (find-lib-dir)) lib-name))
 
   ;; Compile the softposit C library and wait for the make command to finish
-  (call-and-wait (string-append "make -C " (path->string build-location) " SOFTPOSIT_OPTS=\"$(SOFTPOSIT_OPTS) -fPIC\""))
+  (call-and-wait (string-append "make -C " (path->string build-location) " SOFTPOSIT_OPTS=\"$(SOFTPOSIT_OPTS) OPTIMISATION=\"-O2 -march=native\" -fPIC\""))
   (call-and-wait (string-append "gcc " (path->string build-location) "*.o -shared -o " (path->string lib-location)))
   (call-and-wait (string-append "make clean -C " (path->string build-location))))
